b364569b1427d1fb61452732a56a83a8b9995924
kbijker <k.e.bijker@pl.hanze.nl>
2022-03-01 00:33:50 +0100

    Update programs by kbijker 2022-03-01 00:33:48.146096

--- venv/Search_google1.py
+++ venv/Search_google1.py
@@ -4,6 +4,10 @@ import urllib
 from googlesearch import search
 from bs4 import BeautifulSoup
 import requests
+import os, time
+from datetime import datetime
+
+dateTimeObj = datetime.now()
 
 #zoekterm = 'Kobus Bijker'
 #woonplaats = 'Winsum'
@@ -16,8 +20,9 @@ werkgever = input('Werkgever/School (?=niet weten of op zoeken): ')
 exactname = '['+zoekterm+']'
 results_EN = search(zoekterm, lang='english', num=10, stop=10,)
 Res_Alg = []
+print(f'OSINT scan via Google advanced Search wordt uitgevoerd voor {zoekterm}...')
 for url in results_EN:
-    if not 'crdev' in url: Res_Alg.append(url)
+    if not 'crdev' in url: Res_Alg.append(url) #cookie websites negeren
     #print(url)
 
 results_NL = search(exactname, lang='dutch', num=10, stop=10,)
@@ -27,13 +32,14 @@ for url in results_NL:
         Res_NL.append(url)
         if not 'crdev' in url: Res_Alg.append(url)
         #print(url)
-
+print('Nog even geduld aub...websites worden gescand..')
 zoekterm2 = exactname +' AND '+ woonplaats
 results_WP = search(zoekterm2, lang='dutch', num=10, stop=10,)
 Res_WP = []
 count = 0
+
 for url in results_WP:
-    #print(url)
+
     if url not in Res_Alg:
         Res_WP.append(url)
         if count < 2: Res_Alg.append(url)
@@ -43,6 +49,7 @@ zoekterm3 = exactname +' AND '+ werkgever
 results_WG = search(zoekterm3, lang='dutch', num=10, stop=10,)
 Res_WG = []
 count = 0
+print('Nog even geduld aub.....')
 for url in results_WG:
     #print(url)
     if url not in Res_Alg:
@@ -98,7 +105,7 @@ for url in Res_Alg:
                 for letter in Woord:
                     if letter in Hoofdletters and len(Woord) > 5:
                         Termen.append(Woord)
-        Termen_url[url]= Termen
+        Termen_url[url]= (Termen, content)
 
 
     except:
@@ -106,3 +113,21 @@ for url in Res_Alg:
 
 print(Termen_url)
 print(TekstenInteressant)
+
+#Rapport wegschrijven
+file = f'c:/OSINT/rapport_{zoekterm}.txt'
+with open(file, 'w') as f:
+    f.write(f'OSINT rapport voor {zoekterm}:\n')
+    f.write(f'Gevonden websites:\n')
+
+    for url in Res_Alg:
+        f.write(f'{url}\n')
+    f.write('-------------------------------------------------------\n')
+    f.write(f'Diepgaander info over de gevonden sites:\n')
+    for url, termen_cont in Termen_url.items():
+        f.write(f'site: {url}:\n')
+        f.write(f'titel info: {termen_cont[1]}\n')
+        f.write(f'Interessante termen: {termen_cont[0]}\n')
+        f.write('-------------------------------------------------------')
+    f.close()
+print(f'Rapport {file} is aangemaakt')
